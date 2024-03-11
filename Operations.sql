1cc5ddb68b8b63128d58494d747ec16a174ea0c3
Abdullah Ayman Zayed <84142402+AbdullahKhkmba@users.noreply.github.com>
2024-03-11 14:12:33 +0200

    Update Operations.sql

--- Operations.sql
+++ Operations.sql
@@ -13,7 +13,7 @@ WHERE T1.nationality = T2.nationality
     AND T2.FID = 'F1'
     AND T1.age < 18;
 
--- List passengers who traveled from airport X to airport Y on some time interval.
+-- List all minor passengers on a particular flight.
 SELECT T1.Fname, T1.Lname 
 FROM passenger T1, book T2, Flight T3
 WHERE T1.nationality = T2.nationality 
@@ -23,3 +23,11 @@ WHERE T1.nationality = T2.nationality
     AND T3.AID_destination = 'A2'
     AND T3.departure_time BETWEEN '10-march-2024' AND '16-march-2024'
     AND T3.arrival_time BETWEEN '10-march-2024' AND '16-march-2024';
+
+-- Count number of empty seats on a flight
+SELECT economy_class_capacity + first_class_capacity - (SELECT COUNT(FID)
+ FROM Book
+ WHERE FID = 'F2')
+AS Total_number_of_seats
+FROM Aircraft
+WHERE FID = 'F2';
